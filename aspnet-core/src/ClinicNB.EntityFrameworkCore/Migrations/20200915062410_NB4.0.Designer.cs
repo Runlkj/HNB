d0e033fd25a1cf8ad796948fd054aae12fc71688
admin <admin@CHU>
2020-09-15 14:41:51 +0800

    gy

--- aspnet-core/src/ClinicNB.EntityFrameworkCore/Migrations/20200915062410_NB4.0.Designer.cs
+++ aspnet-core/src/ClinicNB.EntityFrameworkCore/Migrations/20200915062410_NB4.0.Designer.cs
@@ -4,14 +4,16 @@ using ClinicNB.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClinicNB.Migrations
 {
     [DbContext(typeof(ClinicNBDbContext))]
-    partial class ClinicNBDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200915062410_NB4.0")]
+    partial class NB40
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/ClinicNB.EntityFrameworkCore/Migrations/20200915062410_NB4.0.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClinicNB.Migrations
+{
+    public partial class NB40 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CaseInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    BGuo = table.Column<string>(nullable: true),
+                    BJia = table.Column<string>(nullable: true),
+                    BGe = table.Column<string>(nullable: true),
+                    BFu = table.Column<string>(nullable: true),
+                    BFa = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CaseInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChineseInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    CDan = table.Column<int>(nullable: false),
+                    CNum = table.Column<int>(nullable: false),
+                    CUserType = table.Column<int>(nullable: false),
+                    CPin = table.Column<int>(nullable: false),
+                    CSum = table.Column<decimal>(nullable: false),
+                    UPId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChineseInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MestInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    CDan = table.Column<int>(nullable: false),
+                    CDay = table.Column<int>(nullable: false),
+                    CPing = table.Column<int>(nullable: false),
+                    CUserType = table.Column<int>(nullable: false),
+                    CPin = table.Column<int>(nullable: false),
+                    CSum = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MestInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PinInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    PName = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PinInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    UName = table.Column<string>(maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CaseInfo");
+
+            migrationBuilder.DropTable(
+                name: "ChineseInfo");
+
+            migrationBuilder.DropTable(
+                name: "MestInfo");
+
+            migrationBuilder.DropTable(
+                name: "PinInfo");
+
+            migrationBuilder.DropTable(
+                name: "UserInfo");
+        }
+    }
+}
